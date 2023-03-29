a6c7389ae28ace31e0f2652e4ddafd5b972906fa
RMRASTRORASTREAMENTO <129010313+RMRASTRORASTREAMENTO@users.noreply.github.com>
2023-03-29 08:01:58 -0400

    Add files via upload

---
+++ rmrastro/scripts/script.js
@@ -0,0 +1,44 @@
+var menuScrollOffset = 60;
+var packageNUM = 1;
+var domain = '6408b6faa05fa.site123.me';
+var languageCode = 'pt';
+var multiLanCode = '';
+var globalLanguageChildLan = '';
+var websiteLanguageCode = '';
+var websiteLanguageCountryCode = '';
+var websiteLanguageCountryFullCode = '';
+var IsManagment = '1';
+var isMinimize = 1;
+var footerCustomTextJson = {
+     "mailingList": {
+          "placeHolder": "Digite seu e-mail",
+          "button": "Assinar"
+     },
+     "cookieConsent": {
+          "text": "Este site usa cookies para garantir que voc\u00ea tenha a melhor experi\u00eancia no nosso site",
+          "button": "Entendi!"
+     },
+     "privacy": {
+          "title": "Privacidade"
+     },
+     "terms": {
+          "title": "Termos"
+     }
+};
+var websiteCustomSearchPlaceHolder = "Digite sua pergunta";
+
+var $GLOBALS = {
+     'cdn-user-files': 'https://static.s123-cdn-static-d.com',
+     'cdn-user-videos-files': 'https://cdn-media.f-static.net',
+     'maps-display-domain': 'https://maps-cdn.site123.com',
+     'cdn-system-files': 'https://cdn-cms-s.f-static.net',
+     'cdn-images-files': 'https://static1.s123-cdn-static-a.com',
+     'v-cache': 'n82724',
+     'is_local_server': false,
+     't_direction': "ltr"
+};
+var longFreeCustomer = true;
+// set progressive web applications settings object for published websites only
+var pwaSettings = {
+     enableMessage: false,
+};
